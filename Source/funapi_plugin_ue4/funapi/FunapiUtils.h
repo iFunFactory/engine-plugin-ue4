bcc5e11267f932184b016fbd66afeb8b0040f1c5
Eunhee <arin@ifunfactory.com>
2015-10-12 10:28:46 +0900

    string format 함수 추가
    Change-Id: I6bfac0aaf065b23e1fc3e53fe1a2c91095fd1f93

--- Source/funapi_plugin_ue4/funapi/FunapiUtils.h
+++ Source/funapi_plugin_ue4/funapi/FunapiUtils.h
@@ -6,29 +6,48 @@
 
 #pragma once
 
-#if PLATFORM_WINDOWS
+#include <memory>
+#include <string>
 
+#if PLATFORM_WINDOWS
 #include <stdint.h>
 
 #pragma warning(disable:4996)
+#endif
 
-namespace fun
+namespace fun {
+
+#if PLATFORM_WINDOWS
+
+struct IOVec
 {
-    struct IOVec
-    {
-        uint8_t* iov_base;
-        size_t iov_len;
-    };
+    uint8_t* iov_base;
+    size_t iov_len;
+};
 
-    #define iovec     IOVec
-    #define ssize_t   size_t
+#define iovec     IOVec
+#define ssize_t   size_t
 
-    #define access    _access
-    #define snprintf  _snprintf
+#define access    _access
+#define snprintf  _snprintf
 
-    #define F_OK      0
+#define F_OK      0
 
-    #define mkdir(path, mode)    _mkdir(path)
-} // namespace fun
+#define mkdir(path, mode)    _mkdir(path)
 
 #endif // PLATFORM_WINDOWS
+
+
+// Format string
+typedef std::string string;
+
+template <typename ... Args>
+string str_fmt(const char* fmt, Args... args)
+{
+  size_t length = snprintf(nullptr, 0, fmt, args...) + 1;
+  std::unique_ptr<char[]> buf(new char[length]);
+  snprintf(buf.get(), length, fmt, args...);
+  return string(buf.get(), buf.get() + length - 1);
+}
+
+} // namespace fun

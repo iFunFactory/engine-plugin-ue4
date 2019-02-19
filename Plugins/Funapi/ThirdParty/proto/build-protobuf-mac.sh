0fa8f29ddf913b27d2a3e843fa121acc97b0dc3f
sungjin.bae <sungjin.bae@ifunfactory.com>
2019-02-19 16:52:23 +0900

    Funapi 플러그인: protobuf 빌드스크립트 기능추가.
    사용자가 소스빌드시 Input/Output director 를 지정해서 빌드하게 변경.
    std 컨테이너들을 fun 컨테이너로 변경.
    
    Change-Id: I235fd134fede9c72eadb03434e94e935ab03c391

---
+++ Plugins/Funapi/ThirdParty/proto/build-protobuf-mac.sh
@@ -0,0 +1,79 @@
+# Copyright (C) 2019 iFunFactory Inc. All Rights Reserved.
+#
+# This work is confidential and proprietary to iFunFactory Inc. and
+# must not be used, disclosed, copied, or distributed without the prior
+# consent of iFunFactory Inc.
+
+echo "Start build .proto files"
+
+# Setting user project name
+PROJECT_NAME=funapi_plugin_ue4
+PROJECT_SOURCE_DIR=../../../../Source
+
+#############################################
+# Build plugin's proto file. DO NOT EDIT!
+#############################################
+
+./protoc --cpp_out=dllexport_decl=FUNAPI_API:../../Source/Funapi/Public \
+  funapi/management/maintenance_message.proto \
+  funapi/network/fun_message.proto \
+  funapi/network/ping_message.proto \
+  funapi/service/multicast_message.proto \
+  funapi/service/redirect_message.proto \
+  funapi/distribution/fun_dedicated_server_rpc_message.proto
+
+mv -f ../../Source/Funapi/Public/funapi/management/maintenance_message.pb.cc \
+  ../../Source/Funapi/Private/funapi/management
+
+mv -f ../../Source/Funapi/Public/funapi/network/fun_message.pb.cc \
+  ../../Source/Funapi/Private/funapi/network
+
+mv -f ../../Source/Funapi/Public/funapi/network/ping_message.pb.cc \
+  ../../Source/Funapi/Private/funapi/network
+
+mv -f ../../Source/Funapi/Public/funapi/service/multicast_message.pb.cc \
+  ../../Source/Funapi/Private/funapi/service
+
+mv -f ../../Source/Funapi/Public/funapi/service/redirect_message.pb.cc \
+  ../../Source/Funapi/Private/funapi/service
+
+mv -f ../../Source/Funapi/Public/funapi/distribution/fun_dedicated_server_rpc_message.pb.cc  \
+  ../../Source/Funapi/Private/funapi/distribution/
+
+#############################################
+# Build test proto files.
+#############################################
+
+# Test echo message file build
+./protoc --cpp_out=${PROJECT_SOURCE_DIR}/${PROJECT_NAME} test_messages.proto
+
+# Test UE4 dedicated rpc file build
+./protoc --cpp_out=${PROJECT_SOURCE_DIR}/${PROJECT_NAME} test_dedicated_server_rpc_messages.proto
+
+
+#############################################
+# Build user's proto files.
+#############################################
+
+# NOTE(sungjin)
+# Build .proto files in {Project/Source} directory.
+
+## Build your custom .proto file, Please Edit Path
+#USER_PROTO_FILE_INPUT_PATH=${PROJECT_SOURCE_DIR}/${PROJECT_NAME}
+#USER_PROTO_FILE_OUT_PATH=${PROJECT_SOURCE_DIR}/${PROJECT_NAME}
+
+
+#FILE_LIST=(`find ${USER_PROTO_FILE_INPUT_PATH} -maxdepth 1 -name "*.proto"`)
+
+## Copy proto files to funapi plugin directory.
+#find ${USER_PROTO_FILE_INPUT_PATH} -maxdepth 1 -name "*.proto" -exec cp {} . \;
+
+## Build proto files to user directory
+#find ./ -maxdepth 1 -name "*.proto" \
+#-exec ./protoc --cpp_out=${USER_PROTO_FILE_OUT_PATH} {} \;
+
+## Remove proto files
+#for value in "${FILE_LIST[@]}"; do
+#    name=$(basename "$value" ".proto")
+#    rm -f $name.proto
+#done
---
+++ Plugins/Funapi/ThirdParty/proto/build-protobuf-win.bat
@@ -0,0 +1,89 @@
+:: Copyright (C) 2019 iFunFactory Inc. All Rights Reserved.
+::
+:: This work is confidential and proprietary to iFunFactory Inc. and
+:: must not be used, disclosed, copied, or distributed without the prior
+:: consent of iFunFactory Inc.
+
+@ECHO OFF
+ECHO "Start build .proto files"
+
+:: Setting user project name
+Set PROJECT_NAME=funapi_plugin_ue4
+Set PROJECT_SOURCE_DIR=..\..\..\..\Source
+
+:::::::::::::::::::::::::::::::::::::::::::::
+:: Build plugin's proto file. DO NOT EDIT!
+:::::::::::::::::::::::::::::::::::::::::::::
+
+protoc.exe --cpp_out=dllexport_decl=FUNAPI_API:..\..\Source\Funapi\Public ^
+    funapi\management\maintenance_message.proto ^
+    funapi\network\fun_message.proto ^
+    funapi\network\ping_message.proto ^
+    funapi\service\multicast_message.proto ^
+    funapi\service\redirect_message.proto ^
+    funapi\distribution\fun_dedicated_server_rpc_message.proto
+
+mv -f ..\..\Source\Funapi\Public\funapi\management\maintenance_message.pb.cc ^
+    ..\..\Source\Funapi\Private\funapi\management
+
+mv -f ..\..\Source\Funapi\Public\funapi\network\fun_message.pb.cc ^
+    ..\..\Source\Funapi\Private\funapi\network
+
+mv -f ..\..\Source\Funapi\Public\funapi\network\ping_message.pb.cc ^
+    ..\..\Source\Funapi\Private\funapi\network
+
+mv -f ..\..\Source\Funapi\Public\funapi\service\multicast_message.pb.cc ^
+    ..\..\Source\Funapi\Private\funapi\service
+
+mv -f ..\..\Source\Funapi\Public\funapi\service\redirect_message.pb.cc ^
+    ..\..\Source\Funapi\Private\funapi\service
+
+mv -f ..\..\Source\Funapi\Public\funapi\distribution\fun_dedicated_server_rpc_message.pb.cc  ^
+    ..\..\Source\Funapi\Private\funapi\distribution
+
+:::::::::::::::::::::::::::::::::::::::::::::
+:: Build test proto files.
+:::::::::::::::::::::::::::::::::::::::::::::
+
+:: Test echo message file build
+protoc.exe --cpp_out=%PROJECT_SOURCE_DIR%\%PROJECT_NAME% test_messages.proto
+
+:: Test UE4 dedicated rpc file build
+protoc --cpp_out=%PROJECT_SOURCE_DIR%\%PROJECT_NAME% test_dedicated_server_rpc_messages.proto
+
+:::::::::::::::::::::::::::::::::::::::::::::
+:: Build user's proto files.
+:::::::::::::::::::::::::::::::::::::::::::::
+
+:: NOTE(sungjin)
+:: Build .proto files in {Project/Source} directory.
+
+::SET ROOT_PATH=%~dp0
+
+:: Build your custom .proto file, Please Edit Path
+::Set USER_PROTO_FILE_INPUT_PATH=%ROOT_PATH%\%PROJECT_SOURCE_DIR%\%PROJECT_NAME%
+::Set USER_PROTO_FILE_OUT_PATH=%ROOT_PATH%\%PROJECT_SOURCE_DIR%\%PROJECT_NAME%
+
+::setLocal enableDelayedExpansion
+::set n=0
+
+:::: Copy proto files to funapi plugin directory.
+::FOR %%I IN (%USER_PROTO_FILE_INPUT_PATH%\*.proto) DO (
+::    :: save copyed files name
+::    set /a n+=1
+::    set FILE_LIST!n!=%%I
+::    :: copy file
+::    copy %%I .
+::)
+
+:::: Build proto files to user directory
+::FOR %%I IN (*.proto) DO (
+::    protoc --cpp_out=%USER_PROTO_FILE_OUT_PATH% %%I
+::)
+
+:::: Remove copyed proto files
+::FOR /L %%i in (1,1,!n!) Do (
+::   FOR /F %%a in ("!FILE_LIST%%i!") DO (
+::    DEL /F %%~na%%~xa
+::   )
+::)

317a3a989783ca6b278b2cfa7a1645c9c95bf507
Sungjin Bae <sungjin.bae@ifunfactory.com>
2019-04-22 01:54:06 +0000

    Funapi 플러그인: FunapiPlugin 내부에서 사용하는 .proto 파일 replace 기능 추가
    플러그인 내부에서 사용하는 메세지의 replace 기능을 복구한다.
    
    This reverts commit 1ceebc2a3da7686f87bc86634017d56193b2a0f4.
    
    Change-Id: Ibed16076ecdab024818c5fa86cd3ae92a555a5c1

--- Plugins/Funapi/ThirdParty/proto/build-protobuf-mac.sh
+++ Plugins/Funapi/ThirdParty/proto/build-protobuf-mac.sh
@@ -12,6 +12,100 @@ echo "Start build .proto files"
 PROJECT_NAME=funapi_plugin_ue4
 PROJECT_SOURCE_DIR=../../../../Source
 
+# NOTE: Avoid IOS Heap curroption
+# pb.cc, pb.h 파일의 std::{container} 를 fun::{container} 로 변경할 수 있습니다.
+# 사용시 주의 점은 아래 링크를 참고해 주세요
+# https://www.ifunfactory.com/engine/documents/reference/ko/client-plugin.html#iphone-xs
+#############################################
+# Replace funtion
+#############################################
+
+ReplaceStdToFun()
+{
+  set -e
+
+  RelaceFile=$1
+  mono replace.exe "\bstd::ostringstream\b" "FUN_OSTRINGSTREAM" $RelaceFile
+  mono replace.exe "\bostringstream\b" "FUN_OSTRINGSTREAM" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_OSTRINGSTREAM_" "ostringstream_" $RelaceFile
+
+  mono replace.exe "\bstd::istringstream\b" "FUN_ISTRINGSTREAM" $RelaceFile
+  mono replace.exe "\bistringstream\b" "FUN_ISTRINGSTREAM" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_ISTRINGSTREAM_" "istringstream_" $RelaceFile
+
+  mono replace.exe "\bstd::stringstream\b" "FUN_STRINGSTREAM" $RelaceFile
+  mono replace.exe "\bstringstream\b" "FUN_STRINGSTREAM" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_STRINGSTREAM_" "stringstream_" $RelaceFile
+
+  mono replace.exe "\bstd::string\b" "FUN_STRING" $RelaceFile
+  mono replace.exe "\bstring\b" "FUN_STRING" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_STRING_" "string_" $RelaceFile
+
+  mono replace.exe "\bstd::vector\b" "FUN_VECTOR" $RelaceFile
+  mono replace.exe "\bvector\b" "FUN_VECTOR" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_VECTOR_" "vector_" $RelaceFile
+
+  mono replace.exe "\bstd::deque\b" "FUN_DEQUE" $RelaceFile
+  mono replace.exe "\bdeque\b" "FUN_DEQUE" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_DEQUE_" "deque_" $RelaceFile
+
+  mono replace.exe "\bstd::queue\b" "FUN_QUEUE" $RelaceFile
+  mono replace.exe "\bqueue\b" "FUN_QUEUE" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_QUEUE_" "queue_" $RelaceFile
+
+  mono replace.exe "\bstd::set\b" "FUN_SET" $RelaceFile
+  mono replace.exe "\bset\b" "FUN_SET" $RelaceFile
+  mono replace.exe "\bFUN_SET_" "set_" $RelaceFile
+
+  mono replace.exe "\bstd::unordered_set\b" "FUN_UNORDERED_SET" $RelaceFile
+  mono replace.exe "\bunordered_set\b" "FUN_UNORDERED_SET" $RelaceFile
+  mono replace.exe "\bFUN_UNORDERED_SET_" "unordered_set_" $RelaceFile
+
+  mono replace.exe "\bstd::map\b" "FUN_MAP" $RelaceFile
+  mono replace.exe "\bmap\b" "FUN_MAP" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_MAP_" "map_" $RelaceFile
+
+  mono replace.exe "\bstd::unordered_map\b" "FUN_UNORDERED_MAP" $RelaceFile
+  mono replace.exe "\bunordered_map\b" "FUN_UNORDERED_MAP" $RelaceFile
+  # remove xx_ word
+  mono replace.exe "\bFUN_UNORDERED_MAP_" "unordered_map_" $RelaceFile
+
+  mono replace.exe "\bFUN_OSTRINGSTREAM" "fun::ostringstream" $RelaceFile
+  mono replace.exe "\bFUN_ISTRINGSTREAM" "fun::istringstream" $RelaceFile
+  mono replace.exe "\bFUN_STRINGSTREAM" "fun::stringstream" $RelaceFile
+  mono replace.exe "\bFUN_STRING" "fun::string" $RelaceFile
+
+  mono replace.exe "\bFUN_VECTOR" "fun::vector" $RelaceFile
+  mono replace.exe "\bFUN_DEQUE" "fun::deque" $RelaceFile
+  mono replace.exe "\bFUN_QUEUE" "fun::queue" $RelaceFile
+  mono replace.exe "\bFUN_SET" "fun::set" $RelaceFile
+  mono replace.exe "\bFUN_UNORDERED_SET" "fun::unordered_set" $RelaceFile
+  mono replace.exe "\bFUN_MAP" "fun::map" $RelaceFile
+  mono replace.exe "\bFUN_UNORDERED_MAP" "fun::unordered_map" $RelaceFile
+
+  mono replace.exe "#include <fun::ostringstream>" "#include <ostringstream>" $RelaceFile
+  mono replace.exe "#include <fun::istringstream>" "#include <istringstream>" $RelaceFile
+  mono replace.exe "#include <fun::stringstream>" "#include <stringstream>" $RelaceFile
+  mono replace.exe "#include <fun::string>" "#include <string>" $RelaceFile
+  mono replace.exe "#include <fun::vector>" "#include <vector>" $RelaceFile
+  mono replace.exe "#include <fun::deque>" "#include <deque>" $RelaceFile
+  mono replace.exe "#include <fun::queue>" "#include <queue>" $RelaceFile
+  mono replace.exe "#include <fun::set>" "#include <set>" $RelaceFile
+  mono replace.exe "#include <fun::map>" "#include <map>" $RelaceFile
+  mono replace.exe "#include <fun::unordered_map>" "#include <unordered_map>" $RelaceFile
+
+  echo "Replaced std container to fun container. FileName: $RelaceFile"
+}
+export -f ReplaceStdToFun
+
 #############################################
 # Build plugin's proto file. DO NOT EDIT!
 #############################################
@@ -48,11 +142,17 @@ mv -f ../../Source/Funapi/Public/funapi/distribution/fun_dedicated_server_rpc_me
 
 # Test echo message file build
 ./protoc --cpp_out=${PROJECT_SOURCE_DIR}/${PROJECT_NAME} test_messages.proto
-find ${PROJECT_SOURCE_DIR}/${PROJECT_NAME} -maxdepth 1 -name "test_messages.pb.*"
+
+# NOTE: Avoid IOS Heap curroption
+# find ${PROJECT_SOURCE_DIR}/${PROJECT_NAME} -maxdepth 1 -name "test_messages.pb.*" \
+#   -exec bash -c 'ReplaceStdToFun "$0"' {} \;
 
 # Test UE4 dedicated rpc file build
 ./protoc --cpp_out=${PROJECT_SOURCE_DIR}/${PROJECT_NAME} test_dedicated_server_rpc_messages.proto
-find ${PROJECT_SOURCE_DIR}/${PROJECT_NAME} -maxdepth 1 -name "test_dedicated_server_rpc_messages.pb.*"
+
+# NOTE: Avoid IOS Heap curroption
+# find ${PROJECT_SOURCE_DIR}/${PROJECT_NAME} -maxdepth 1 -name "test_dedicated_server_rpc_messages.pb.*" \
+#   -exec bash -c 'ReplaceStdToFun "$0"' {} \;
 
 
 #############################################
@@ -79,3 +179,20 @@ for value in "${FILE_LIST[@]}"; do
     name=$(basename "$value" ".proto")
     rm -f $name.proto
 done
+
+
+# NOTE: Avoid IOS Heap curroption
+#############################################
+# Replace std container
+#############################################
+
+# find ${USER_PROTO_FILE_OUT_PATH} -maxdepth 1 -name "*.pb.*" \
+#   -exec bash -c 'ReplaceStdToFun "$0"' {} \;
+
+
+# NOTE(sungjin): replace std to fun FunapiPlugin Protobuf
+find ${PWD}/../../Source/Funapi/Public/funapi -name "*.pb.*" \
+  -exec bash -c 'ReplaceStdToFun "$0"' {} \;
+
+find ${PWD}/../../Source/Funapi/Private/funapi -name "*.pb.*" \
+  -exec bash -c 'ReplaceStdToFun "$0"' {} \;
--- Plugins/Funapi/ThirdParty/proto/build-protobuf-win.bat
+++ Plugins/Funapi/ThirdParty/proto/build-protobuf-win.bat
@@ -87,4 +87,118 @@ FOR /L %%i IN (1,1,!n!) Do (
     )
 )
 
+
+:::::::::::::::::::::::::::::::::::::::::::::
+:: Replace std container
+:::::::::::::::::::::::::::::::::::::::::::::
+FOR /R %ROOT_PATH%..\..\Source\Funapi\Private\funapi %%i IN (*.pb.*) DO (
+    CALL :ReplaceStdToFun %%i
+)
+
+FOR /R %ROOT_PATH%..\..\Source\Funapi\Public\funapi %%i IN (*.pb.*) DO (
+    CALL :ReplaceStdToFun %%i
+)
+
+:: CD %USER_PROTO_FILE_OUT_PATH%
+:: FOR %%i IN (*.pb.*) DO (
+::     CALL :ReplaceStdToFun %cd%\%%i
+:: )
+
+EXIT /B 0
+
+:::::::::::::::::::::::::::::::::::::::::::::
+:: Replace funtion
+:::::::::::::::::::::::::::::::::::::::::::::
+
+:ReplaceStdToFun
+    %~dp0replace.exe "\bstd::ostringstream\b" "FUN_OSTRINGSTREAM" %~1
+    %~dp0replace.exe "\bostringstream\b" "FUN_OSTRINGSTREAM" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_OSTRINGSTREAM_" "ostringstream_" %~1
+
+    %~dp0replace.exe "\bstd::istringstream\b" "FUN_ISTRINGSTREAM" %~1
+    %~dp0replace.exe "\bistringstream\b" "FUN_ISTRINGSTREAM" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_ISTRINGSTREAM_" "istringstream_" %~1
+
+
+    %~dp0replace.exe "\bstd::stringstream\b" "FUN_STRINGSTREAM" %~1
+    %~dp0replace.exe "\bstringstream\b" "FUN_STRINGSTREAM" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_STRINGSTREAM_" "stringstream_" %~1
+
+
+    %~dp0replace.exe "\bstd::string\b" "FUN_STRING" %~1
+    %~dp0replace.exe "\bstring\b" "FUN_STRING" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_STRING_" "string_" %~1
+
+
+    %~dp0replace.exe "\bstd::vector\b" "FUN_VECTOR" %~1
+    %~dp0replace.exe "\bvector\b" "FUN_VECTOR" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_VECTOR_" "vector_" %~1
+
+
+    %~dp0replace.exe "\bstd::deque\b" "FUN_DEQUE" %~1
+    %~dp0replace.exe "\bdeque\b" "FUN_DEQUE" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_DEQUE_" "deque_" %~1
+
+
+    %~dp0replace.exe "\bstd::queue\b" "FUN_QUEUE" %~1
+    %~dp0replace.exe "\bqueue\b" "FUN_QUEUE" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_QUEUE_" "queue_" %~1
+
+
+    %~dp0replace.exe "\bstd::set\b" "FUN_SET" %~1
+    %~dp0replace.exe "\bset\b" "FUN_SET" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_SET_" "set_" %~1
+
+    %~dp0replace.exe "\bstd::unordered_set\b" "FUN_UNORDERED_SET" %~1
+    %~dp0replace.exe "\bunordered_set\b" "FUN_UNORDERED_SET" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_UNORDERED_SET_" "unordered_set_" %~1
+
+
+    %~dp0replace.exe "\bstd::map\b" "FUN_MAP" %~1
+    %~dp0replace.exe "\bmap\b" "FUN_MAP" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_MAP_" "map_" %~1
+
+
+    %~dp0replace.exe "\bstd::unordered_map\b" "FUN_UNORDERED_MAP" %~1
+    %~dp0replace.exe "\bunordered_map\b" "FUN_UNORDERED_MAP" %~1
+    :: remove xx_ word
+    %~dp0replace.exe "\bFUN_UNORDERED_MAP_" "unordered_map_" %~1
+
+
+    %~dp0replace.exe "\bFUN_OSTRINGSTREAM" "fun::ostringstream" %~1
+    %~dp0replace.exe "\bFUN_ISTRINGSTREAM" "fun::istringstream" %~1
+    %~dp0replace.exe "\bFUN_STRINGSTREAM" "fun::stringstream" %~1
+    %~dp0replace.exe "\bFUN_STRING" "fun::string" %~1
+
+
+    %~dp0replace.exe "\bFUN_VECTOR" "fun::vector" %~1
+    %~dp0replace.exe "\bFUN_DEQUE" "fun::deque" %~1
+    %~dp0replace.exe "\bFUN_QUEUE" "fun::queue" %~1
+    %~dp0replace.exe "\bFUN_SET" "fun::set" %~1
+    %~dp0replace.exe "\bFUN_UNORDERED_SET" "fun::unordered_set" %~1
+    %~dp0replace.exe "\bFUN_MAP" "fun::map" %~1
+    %~dp0replace.exe "\bFUN_UNORDERED_MAP" "fun::unordered_map" %~1
+
+
+    %~dp0replace.exe "#include <fun::ostringstream>" "#include <ostringstream>" %~1
+    %~dp0replace.exe "#include <fun::istringstream>" "#include <istringstream>" %~1
+    %~dp0replace.exe "#include <fun::stringstream>" "#include <stringstream>" %~1
+    %~dp0replace.exe "#include <fun::string>" "#include <string>" %~1
+    %~dp0replace.exe "#include <fun::vector>" "#include <vector>" %~1
+    %~dp0replace.exe "#include <fun::deque>" "#include <deque>" %~1
+    %~dp0replace.exe "#include <fun::queue>" "#include <queue>" %~1
+    %~dp0replace.exe "#include <fun::set>" "#include <set>" %~1
+    %~dp0replace.exe "#include <fun::map>" "#include <map>" %~1
+    %~dp0replace.exe "#include <fun::unordered_map>" "#include <unordered_map>" %~1
+    ECHO "Replaced std container to fun container. FileName: %~1"
 EXIT /B 0

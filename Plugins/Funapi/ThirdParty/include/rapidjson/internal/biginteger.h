9401fd9f782c301a983727e312afa8c47325da93
sungjin.bae <sungjin.bae@ifunfactory.com>
2019-06-11 16:56:45 +0900

    Funapi 플러그인: Win32 환경에서 rapidjson _GNUC_ 정의 되지않은 컴파일러 경고 제거
    Win32 환경에서 rapidjson 파일이 컴파일 될때 C4668 error 가 발생한다.
    
    코드에서 각 컴파일러 및 아키텍쳐를 확인하는 부분에서 발생하는 에러로
    에러가 발생하는 biginteger.h, diyfp.h 파일에만 경고를 무시하는 컴파일러 옵션을 넣는다.
    
    Change-Id: Ibf42b779d6fc4e61c67680b9685baa48ec32d417

--- Plugins/Funapi/ThirdParty/include/rapidjson/internal/biginteger.h
+++ Plugins/Funapi/ThirdParty/include/rapidjson/internal/biginteger.h
@@ -1,5 +1,5 @@
 // Tencent is pleased to support the open source community by making RapidJSON available.
-// 
+//
 // Copyright (C) 2015 THL A29 Limited, a Tencent company, and Milo Yip. All rights reserved.
 //
 // Licensed under the MIT License (the "License"); you may not use this file except
@@ -7,9 +7,9 @@
 //
 // http://opensource.org/licenses/MIT
 //
-// Unless required by applicable law or agreed to in writing, software distributed 
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
 #ifndef RAPIDJSON_BIGINTEGER_H_
@@ -19,6 +19,12 @@
 
 #if defined(_MSC_VER) && defined(_M_AMD64)
 #include <intrin.h> // for _umul128
+#endif
+
+// NOTE(Sungjin): Window 32 bit 환경에서 에러를 방지한다.
+#if defined(_MSC_VER) && !defined(_M_AMD64)
+#pragma warning( push )
+#pragma warning( disable : 4668 )
 #endif
 
 RAPIDJSON_NAMESPACE_BEGIN
@@ -52,7 +58,7 @@ public:
     }
 
     BigInteger& operator=(uint64_t u) {
-        digits_[0] = u;            
+        digits_[0] = u;
         count_ = 1;
         return *this;
     }
@@ -85,7 +91,7 @@ public:
             digits_[i] = MulAdd64(digits_[i], u, k, &hi);
             k = hi;
         }
-        
+
         if (k > 0)
             PushBack(k);
 
@@ -108,7 +114,7 @@ public:
             digits_[i] = (p0 & 0xFFFFFFFF) | (p1 << 32);
             k = p1 >> 32;
         }
-        
+
         if (k > 0)
             PushBack(k);
 
@@ -276,5 +282,10 @@ private:
 
 } // namespace internal
 RAPIDJSON_NAMESPACE_END
+
+// NOTE(Sungjin): Window 32 bit 환경에서 에러를 방지한다.
+#if defined(_MSC_VER) && !defined(_M_AMD64)
+#pragma warning( pop )
+#endif
 
 #endif // RAPIDJSON_BIGINTEGER_H_

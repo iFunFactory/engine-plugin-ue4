f9b2a8b5c828f6781780e8133ad0b5d63699f367
sungjin.bae <sungjin.bae@ifunfactory.com>
2019-08-06 17:29:53 +0900

    Funapi 플러그인: Tcp EnablePing 옵션을 사용시 플러그인 내부에서 자동으로 Ping Update.
    Session 의 업데이트를 호출하지 못하는 상황에서 Ping Timeout 이 발생하곤 했는데
    이를 플러그인 내부에서 Ping 을 자동으로 업데이트 하여 Ping Timeout 을 방지.
    
    기존의 FunapiSession::UpdateAll() 혹은 FunapiSession::Update() 의 동작 방식에는 차이가 없고
    Update 호출 하지 못하는 상황을 대비하여 추가된 기능.
    
    Change-Id: Id69b703488224aabd80dc6503323077b9b2cf406

--- Plugins/Funapi/Source/Funapi/Private/funapi_socket_win.cpp
+++ Plugins/Funapi/Source/Funapi/Private/funapi_socket_win.cpp
@@ -98,6 +98,8 @@ fun::string FunapiAddrInfoImpl::GetString() {
 
 class FunapiSocketImpl : public std::enable_shared_from_this<FunapiSocketImpl> {
  public:
+  typedef FunapiTcp::PingHandler PingHandler;
+
   FunapiSocketImpl();
   virtual ~FunapiSocketImpl();
 
@@ -117,6 +119,9 @@ class FunapiSocketImpl : public std::enable_shared_from_this<FunapiSocketImpl> {
   virtual void OnSelect(HANDLE handle) = 0;
 
   HANDLE GetEventHandle();
+
+  void SetPingHandler(const PingHandler &ping_handler);
+  void PingUpdate();
 
  protected:
   bool InitAddrInfo(int socktype,
@@ -140,6 +145,8 @@ class FunapiSocketImpl : public std::enable_shared_from_this<FunapiSocketImpl> {
   struct addrinfo *addrinfo_ = nullptr;
   struct addrinfo *addrinfo_res_ = nullptr;
   HANDLE event_handle_ = nullptr;
+
+  PingHandler ping_handler_;
 };
 
 
@@ -244,6 +251,16 @@ bool FunapiSocketImpl::Select()
         return false;
       }
 
+      // PING
+      if (wait_result == WSA_WAIT_TIMEOUT)
+      {
+        for (auto s : v_select_sockets)
+        {
+          s->PingUpdate();
+        }
+
+        return true;
+      }
 
       // SEND
       DWORD event_index = wait_result - WSA_WAIT_EVENT_0;
@@ -425,6 +442,21 @@ HANDLE FunapiSocketImpl::GetEventHandle() {
 }
 
 
+void FunapiSocketImpl::SetPingHandler(const PingHandler &ping_handler)
+{
+  ping_handler_ = ping_handler;
+}
+
+
+void FunapiSocketImpl::PingUpdate()
+{
+  if (ping_handler_)
+  {
+    ping_handler_();
+  }
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // FunapiTcpImpl implementation.
 
@@ -434,6 +466,7 @@ class FunapiTcpImpl : public FunapiSocketImpl {
   typedef FunapiTcp::RecvHandler RecvHandler;
   typedef FunapiTcp::SendHandler SendHandler;
   typedef FunapiTcp::SendCompletionHandler SendCompletionHandler;
+  typedef FunapiTcp::PingHandler PingHandler;
 
   FunapiTcpImpl();
   virtual ~FunapiTcpImpl();
@@ -1236,6 +1269,13 @@ int FunapiTcp::GetSocket() {
 void FunapiTcp::OnSelect(HANDLE handle) {
   impl_->OnSelect(handle);
 }
+
+
+void FunapiTcp::SetPingHandler(const PingHandler &ping_handler)
+{
+  impl_->SetPingHandler(ping_handler);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // FunapiUdp implementation.
